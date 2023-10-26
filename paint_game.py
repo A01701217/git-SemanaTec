edbe6f9fb74a493bc535d9288ed05a78a5a50b13
[bullet] <A01701217Qitesm.mx>
2023-10-26 12:34:04 -0600

    Juego con las funciones finalizadas para paint

--- paint_game.py
+++ paint_game.py
@@ -47,11 +47,33 @@ def circle_f(start, end):
 
 def rectangle(start, end):
     """Draw rectangle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(2):
+        forward(end.x - start.x)
+        left(90)
+        forward((end.x - start.x)/2)
+        left(90)
+    
+
+    end_fill()
 
 
 def triangle(start, end):
     """Draw triangle from start to end."""
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range (3):
+        forward(end.x - start.x)
+        left(120)
+
+    end_fill()
     pass  # TODO
 
 # Funcion para guardar punto de inicio e indicar la figura a dibujar

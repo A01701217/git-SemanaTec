21dcc2101de304f261be16b473f821dd46ea48d2
Emiliano <a01782190@tec.mx>
2023-10-26 22:17:15 -0600

    Al aceptar este commit se agregan comentarios al juego snake

--- snake_game.py
+++ snake_game.py
@@ -18,19 +18,19 @@ import random
 food = vector(0, 0) #Posicion inicial de la comida
 snake = [vector(10, 0)] #Posicion inicial de la serpiente
 aim = vector(0, -10) #Direccion inicial a la que se dirige la serpiente
-count = vector(0,0)
+count = vector(0,0) #Contador de movimientos para saber a donde se dirigira la comida
 
-def change(x, y):
+def change(x, y): #Funcion para poder cambiar la dirección de la serpiente
     """Change snake direction."""
-    aim.x = x
-    aim.y = y
+    aim.x = x #Dirección en x
+    aim.y = y #Dirección en y
 
-def inside(head):
+def inside(head): #Función para saber si la serpiente esta dentro del mapa
     """Return True if head inside boundaries."""
     return -200 < head.x < 190 and -200 < head.y < 190
 
-def cambiarcolor():
-    numero_aleatorio = random.randint(1, 5) #Generamos un numero aleatorio que representara un color
+def cambiarcolor(): #Funcion para elegir un color aleatorio que tendra la serpiente al iniciar el juego
+    numero_aleatorio = random.randint(1, 5) 
     #Los colores se eligen
     if numero_aleatorio == 1:
         colorserp = 'black'
@@ -46,26 +46,26 @@ def cambiarcolor():
 
 colores = cambiarcolor()  # Genera un color aleatorio único al comienzo del juego
 
-def move():
-    head = snake[-1].copy()
-    head.move(aim)
+def move():  #Funcion la cual indica el como se movera la serpiente
+    head = snake[-1].copy()  #Representa una copia de la cabeza de la serpiente
+    head.move(aim) #direccion a la cual se dirige la serpiente
 
-    if not inside(head) or head in snake:
+    if not inside(head) or head in snake: #Condicion la cual checa si la serpiente esta en los limites o dentro de si misma, si se cumple pierdes.
         square(head.x, head.y, 9, 'red')
         update()
         return
 
-    snake.append(head)
+    snake.append(head) #Se unen las piezas de serpiente
 
-    if head == food:
+    if head == food:#Condicion para saber si se comio comida y alargar la serpiente
         print('Snake:', len(snake))
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
     else:
         snake.pop(0)
 
-    clear()
-    if count.x % 2 == 0:
+    clear() #Se reinicia la pantalla para un refresh
+    if count.x % 2 == 0: #Movimiento de la comida en x
         if -200 < food.x < 190:
             food.x += randrange(-1, 2) * 10
         elif food.x == -200:
@@ -73,7 +73,7 @@ def move():
         elif food.x == 190:
             food.x -= 50
 
-        if -200 < food.y < 190:
+        if -200 < food.y < 190: #Movimiento de la comida en y
             food.y += randrange(-1, 2) * 10
         elif food.y == -200:
             food.y += 50
@@ -83,13 +83,14 @@ def move():
 
     for body in snake:
         square(body.x, body.y, 9, colores)  # Utiliza el color aleatorio generado
-    square(food.x, food.y, 9, 'green')
+    square(food.x, food.y, 9, 'green') 
     update()
     ontimer(move, 100)
 
-setup(420, 420, 370, 0)
-hideturtle()
-tracer(False)
+setup(420, 420, 370, 0) #Canva
+hideturtle() #No se muestra la tortuga
+tracer(False) #no se muestra el como se dibuja
+#Asignacion de teclas
 listen()
 onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
